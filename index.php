d58ced70c311b9f8da98a06c5c8903aac4f6a438
nairhid <nair.hid@gmail.com>
2013-06-26 16:02:45 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>WTGroup | Investigación de Mercado y Opinión Pública</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Investigación de Mercado y Opinión Pública" />
+		<meta name="keywords" content="Mercado, Market, Opinion Publica, Public, Gobierno, Política, Medios, Analisis, Encuestas, Boca de Urna" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/config.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header-wrapper">
+				<header id="header" class="container">
+					<div class="row">
+						<div class="12u">
+
+							<!-- Logo -->
+								<h1><a href="index.php" id="logo"><img src="images/logo_wtgroup_bco.png" height="70px"></a></h1>
+
+							<!-- Nav -->
+								<nav id="nav">
+									<a href="index.php">Inicio</a>
+									<a href="nosotros.php">Nosotros</a>
+									<a href="servicios.php">Servicios</a>
+									<a href="#contacto">Contacto</a>
+									<!--<a href="onecolumn.html">One Column</a>-->
+								</nav>
+
+						</div>
+					</div>
+				</header>
+                </div>
+                <div id="banner-wrapper">
+				<div id="banner">
+					<div class="container">
+						<div class="row">
+							<div class="6u">
+
+								<!-- Banner Copy -->
+                                <h1>ASESORAMIENTO INTEGRAL , INFORMACIÓN CONFIABLE.</h1>
+								<p>Hace casi 20 años que nos especializamos en asesoramiento integral de empresas, organismos públicos y figuras políticas, realizando investigaciones de mercado y opinión pública.</p><p>Brindamos información confiable y con respaldo. Sugerimos y accionamos en Asuntos Públicos facilitando la relación entre el ámbito privado y la esfera estatal.</p>
+								<!-- <a href="#" class="button-big">Contacto</a> -->
+
+							</div>
+							<div class="6u">
+
+								<!-- Banner Image -->
+									<a href="#" class="feature-image"><img src="images/banner1.png" alt="" /></a>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<!-- Features -->
+			<div id="features-wrapper">
+				<div id="features">
+					<div class="container">
+						<div class="row">
+							<div class="3u">
+
+								<!-- Feature #1 -->
+									<section>
+										<a href="servicios.php#asesoramiento" class="feature-image"><img src="images/pic01.png" alt="WTGroup | Asesoramiento Integral" /></a>
+										<a href="servicios.php#asesoramiento"><h2>Asesoramiento Integral</h2></a>
+										<p>
+
+Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500. 
+
+										</p>
+									</section>
+
+							</div>
+							<div class="3u">
+
+								<!-- Feature #2 -->
+									<section>
+										<a href="servicios.php#investigacion" class="feature-image"><img src="images/pic02.png" alt="WTGroup | Investigación y Desarrollo" /></a>
+										<a href="servicios.php#investigacion"><h2>Investigación y Desarrollo</h2></a>
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam nec tellus ut tempus. Etiam ultricies turpis eget nibh vehicula, pharetra varius enim convallis. Sed sodales leo id dui luctus laoreet. 
+										</p>
+									</section>
+
+							</div>
+							<div class="3u">
+
+								<!-- Feature #3 -->
+									<section>
+										<a href="servicios.php#medios" class="feature-image"><img src="images/pic03.png" alt="WTGroup | MMC" /></a>
+										<a href="servicios.php#medios"><h2>Medios Masivos de Comunicación</h2></a>
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam nec tellus ut tempus. Etiam ultricies turpis eget nibh vehicula, pharetra varius enim convallis. Sed sodales leo id dui luctus laoreet. 
+										</p>
+									</section>
+
+							</div>
+							<div class="3u">
+
+								<!-- Feature #4 -->
+									<section>
+										<a href="servicios.php#campana" class="feature-image"><img src="images/pic04.png" alt="WTGroup | Campaña" /></a>
+										<a href="servicios.php#campana"><h2>Campaña</h2></a>
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam nec tellus ut tempus. Etiam ultricies turpis eget nibh vehicula, pharetra varius enim convallis. Sed sodales leo id dui luctus laoreet. 
+										</p>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<!-- Content -->
+			<div id="content-wrapper">
+				<div id="content">
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+
+								<!-- Box #1 -->
+									<section>
+										<header>
+											<h2>Quiénes somos</h2>
+											<h3>Subtítulo informativo</h3>
+										</header>
+										<!--<a href="#" class="feature-image"><img src="images/pic05.jpg" alt="" /></a>-->
+										<p>
+											Somos una empresa con casi 20 años de trayectoria. Nacimos en la Patagonia y junto a nuestros clientes fuimos creciendo y expandiéndonos hasta cubrir  con nuestros estudios, todo el país. Durante este tiempo hemos desarrollado  modelos propios que son de fundamental apoyo y soporte para nuestros clientes a la hora de tomar decisiones.
+
+										</p>
+                                        <a href="nosotros.php">Leer Más</a>
+									</section>
+
+							</div>
+							<div class="4u">
+
+								<!-- Box #2 -->
+									<section>
+										<header>
+											<h2>Qué Hacemos</h2>
+											<h3>Algunos ejemplos de nuestros servicios:</h3>
+										</header>
+										<ul class="check-list">
+											<li>Campañas Electorales</li>
+											<li>Estrategia de Gobierno</li>
+											<li>Seguimiento de Medios</li>
+											<li>Estudios de opinión pública</li>
+										</ul>
+                                        <a href="servicios.php">Más Servicios</a>
+									</section>
+
+							</div>
+
+                            <div class="4u">
+
+    							<!-- Box #3 -->
+									<section>
+										<header>
+											<h2>Proyectos</h2>
+											<h3>Algunos ejemplos</h3>
+										</header>
+										<ul class="link-list first-child">
+											<li>Bancos y entidades  financieras, públicas y privadas.</li>
+											<li>Candidatos a Presidente, Gobernador e Intendente.</li>
+											<li>Gobiernos provinciales y municipales.</li>
+                                            <li>Ministerios, empresas del estado, ONGs y sindicatos.</li>
+											<li>Cooperativas de servicios públicos.</li>
+										</ul>
+                                        <a href="nosotros.php#proyectos">Más casos de éxito</a>
+									</section>
+
+							</div>
+
+						<!--	<div class="4u">
+
+
+									<section>
+										<header>
+											<h2>What People Are Saying</h2>
+											<h3>And a final subheading about our clients</h3>
+										</header>
+										<ul class="quote-list">
+											<li>
+												<img src="images/pic06.jpg" alt="" />
+												<p>"Neque nisidapibus mattis"</p>
+												<span>Jane Doe, CEO of UntitledCorp</span>
+											</li>
+											<li>
+												<img src="images/pic07.jpg" alt="" />
+												<p>"Lorem ipsum consequat!"</p>
+												<span>John Doe, President of FakeBiz</span>
+											</li>
+											<li>
+												<img src="images/pic08.jpg" alt="" />
+												<p>"Magna veroeros amet tempus"</p>
+												<span>Mary Smith, CFO of UntitledBiz</span>
+											</li>
+										</ul>
+									</section>
+
+							</div> -->
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<!-- Footer -->
+			<div id="footer-wrapper">
+				<footer id="footer" class="container">
+					<div class="row">
+						<div class="4u">
+
+						<!--Links -->	 
+								<section>
+									<h2>Sitio</h2>
+
+												<ul class="link-list last-child">
+													<li><a href="#">Inicio</a></li>
+													<li><a href="#">Nosotros</a></li>
+													<li><a href="#">Servicios</a></li>
+													<li><strong>W Tracking Group</strong> <br>
+                                                    Teléfonos: (+54299) 498 2749 / 1649 <br>
+                                                    Email: consultas@wtgroup.com.ar</li>
+												</ul>
+
+                                </section>
+                        </div>      
+                            <!-- Tweets -->   
+                        <div class="4u">    
+                                <section>
+    								<h2>Tweets</h2>
+												<a class="twitter-timeline"  href="https://twitter.com/twitter"  data-widget-id="349567600264609793">Tweets por @twitter</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+                                </section>
+
+                        </div>
+
+                        <div class="4u" id="contacto">
+                            <!-- Contacto -->    
+                                <section>
+        							<h2>Contacto</h2>
+
+
+
+												<?php
+
+   if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+
+
+
+?> 
+
+
+
+<form name="contactame" method="post" action="contacto_form.php"> 
+
+<input type=text name="nombre" size=40 value="Nombre"> 
+
+<br>
+
+<input type=text name=email size=40 value="Email"> 
+
+<br> 
+
+<input type=text name="telefono" size=40 value="Teléfono"> 
+
+<br> 
+
+Comentarios<br> 
+<textarea name=coment cols=64 rows=3></textarea> 
+
+<br> 
+
+<input type=submit value="Enviar"> 
+
+</form> 
+
+                                </section>
+                        </div>        
+
+
+							<!-- Blurb 
+								<section>
+									<h2>An Informative Text Blurb</h2>
+									<p>
+										Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed. Suspendisse eu 
+										varius nibh. Suspendisse vitae magna eget odio amet mollis. Duis neque nisi, 
+										dapibus sed mattis quis, sed rutrum accumsan sed. Suspendisse eu varius nibh 
+										lorem ipsum amet dolor sit amet lorem ipsum consequat gravida justo mollis.
+									</p>
+								</section>-->
+
+						</div>
+					</div>
+				</footer>
+			</div>
+
+		<!-- Copyright -->
+			<div id="copyright">
+				Diseño: <a href="http://nair.somosbanana.com.ar">Banana | Design Studio</a> 
+			</div>
+
+	</body>
+</html>
